48b50fc2dca498884e1b569596ae84f931ec3496
Sean Newman <seangnewman@msn.com>
2018-10-09 13:27:31 -0700

    Initial Commit

---
+++ NullCoalescingOperator/NullCoalescingOperator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedFeatures
+{
+    class NullCoalescingOperator
+    {
+        public static string Demo(string value)
+        {
+            return value ?? string.Empty;
+        }
+
+        public static int Demo(int? value)
+        {
+            int? temp = null;
+            return temp ?? default(int);
+        }
+    }
+}
---
+++ NullCoalescingOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var testing = NullCoalescingOperator.Demo("Harvey Mudd");
+            var intTest = NullCoalescingOperator.Demo(7);
+            Console.WriteLine(testing);
+            Console.WriteLine(8);
+             
+        }
+
+
+    }
+}
